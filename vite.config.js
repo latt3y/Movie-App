d91d92b429be6b6caeb7a4dafe82032c74deb14a
Laurat <l4tt1haga@gmail.com>
2022-02-01 13:46:34 +0100

    vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: 'https://laurathaga.github.io/Movie-App/',
+	base: 'https://github.com/laurathaga/Movie-App.git',
 	plugins: [ react() ]
 });
