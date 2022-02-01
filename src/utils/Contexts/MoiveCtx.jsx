2a8072cb354f424783e34aa2a41884a47301ab7f
Laurat <l4tt1haga@gmail.com>
2022-02-01 15:44:21 +0100

    fixing url

--- src/utils/Contexts/MoiveCtx.jsx
+++ src/utils/Contexts/MoiveCtx.jsx
@@ -5,7 +5,7 @@ export const MovieCtx = React.createContext();
 export const FavMovieCtx = React.createContext();
 export const SetFavMovieCtx = React.createContext();
 
-const url = 'https://fake-movie-api.herokuapp.com/';
+const url = 'https://fake-movie-api.herokuapp.com/movie';
 
 const MovieProvider = ({ children }) => {
 	const [ favorites, setFavorites ] = React.useState([]);
