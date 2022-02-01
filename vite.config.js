c358acaec417f3ad268e898a83def43f0ba3f4e1
Laurat <l4tt1haga@gmail.com>
2022-02-01 15:05:03 +0100

    vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: 'https://vigilant-swanson-b515f7.netlify.app/',
 	plugins: [ react() ]
 });
